3f5ec299c8355148dc4d974cb813f8c368521fae
Reginald <reginaldbeakes@gmail.com>
2022-06-30 18:08:02 -0400

    First Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='sbbtracker_logparser',
+    version='0.1',
+    description='A Storybook Brawl Log Parser',
+    packages=find_packages(),
+)
